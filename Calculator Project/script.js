ba2a69811876c5f075846035c49f89424182ddca
Harish E A <harishkumarea@gmail.com>
2025-08-20 19:37:10 +0530

    Add files via upload

---
+++ Calculator Project/script.js
@@ -0,0 +1,17 @@
+let outputscreen = document.getElementById("output-screen");
+function display(num) {
+  outputscreen.value += num;
+}
+function calculate() {
+  try {
+    outputscreen.value = eval(outputscreen.value);
+  } catch {
+    alert("Invalid");
+  }
+}
+function del() {
+  outputscreen.value = outputscreen.value.slice(0, -1);
+}
+function clearScreen() {
+  outputscreen.value = "";
+}
